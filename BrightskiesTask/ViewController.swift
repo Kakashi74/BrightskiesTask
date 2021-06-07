9fcc029bd037f8a1f278484a5fffe008a8024462
Ahmed Mohamed <AhmedKaKashi74@gmail.com>
2021-06-08 01:14:57 +0200

    Initial Commit

---
+++ BrightskiesTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BrightskiesTask
+//
+//  Created by Ahmed on 6/8/21.
+//  Copyright © 2021 eramint.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
